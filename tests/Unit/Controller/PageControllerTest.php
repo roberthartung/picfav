906e62a90da9b2b83a87fef9fc2a655e46f11348
Robert Hartung <hartung@ibr.cs.tu-bs.de>
2021-07-28 11:27:11 +0200

    first commit: template added

---
+++ tests/Unit/Controller/PageControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OCA\PicFav\Tests\Unit\Controller;
+
+use PHPUnit_Framework_TestCase;
+
+use OCP\AppFramework\Http\TemplateResponse;
+
+use OCA\PicFav\Controller\PageController;
+
+
+class PageControllerTest extends PHPUnit_Framework_TestCase {
+	private $controller;
+	private $userId = 'john';
+
+	public function setUp() {
+		$request = $this->getMockBuilder('OCP\IRequest')->getMock();
+
+		$this->controller = new PageController(
+			'picfav', $request, $this->userId
+		);
+	}
+
+	public function testIndex() {
+		$result = $this->controller->index();
+
+		$this->assertEquals('index', $result->getTemplateName());
+		$this->assertTrue($result instanceof TemplateResponse);
+	}
+
+}
